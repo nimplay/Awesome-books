8e7d16b678943bcee91ed6520dbad11c2d8b1796
nimplay <nimrod7day@gmail.com>
2022-06-27 17:28:17 -0400

    configutarion of button

--- app.js
+++ app.js
@@ -1,35 +1,42 @@
-const deck = document.querySelector('books-container');
-const bookData = document.createElement('div');
-bookData.classList.add('book');
-deck.appendChild(bookData)
+let title = '50 sobras de la cena';
+let autor = 'rosa meltronco';
+let i = 0;
 
 function newBook() {
-    let i = 0;
-    const bookTitle = document.createElement('p');
-    const bookAuthor = document.createElement('p');
-    const removeBtn = document.createElement('button');
-    const line = document.createElement('div');
+  const deck = document.getElementById('books-container');
+  const bookData = document.createElement('div');
+  bookData.className = 'book';
+  deck.appendChild(bookData);
+  const bookTitle = document.createElement('p');
+  const bookAuthor = document.createElement('p');
+  const removeBtn = document.createElement('button');
+  const line = document.createElement('div');
 
-    bookTitle.classList.add('book-title');
-    bookAuthor.classList.add('book-author');
-    removeBtn.classList.add('remove-btn')
-    line.classList.add('line');
+  bookTitle.classList.add('book-title');
+  bookAuthor.classList.add('book-author');
+  removeBtn.classList.add('remove-btn');
+  line.classList.add('line');
 
-    bookTitle.setAttribute('id', `bt${i}`);
-    bookAuthor.setAttribute('id', `ba${i}`);
-    removeBtn.setAttribute('id', `rb${i}`);
+  bookTitle.setAttribute('id', `bt${i}`);
+  bookAuthor.setAttribute('id', `ba${i}`);
+  removeBtn.setAttribute('id', `rb${i}`);
+  i += 1;
 
-    const btId = bookTitle.getAttribute('id');
-    const baId = bookAuthor.getAttribute('id');
-    const rbId = removeBtn.getAttribute('id');
+  const btId = bookTitle.getAttribute('id');
+  const baId = bookAuthor.getAttribute('id');
+  const rbId = removeBtn.getAttribute('id');
 
-    bookData.appendChild(bookTitle);
-    bookData.appendChild(bookAuthor);
-    bookData.appendChild(removeBtn);
-    bookData.appendChild(line);
+  bookData.appendChild(bookTitle);
+  bookData.appendChild(bookAuthor);
+  bookData.appendChild(removeBtn);
+  bookData.appendChild(line);
 
-    document.getElementById('btId').textContent = 'Pepito preguntó'
+  document.getElementById(btId).textContent = title;
+  document.getElementById(baId).textContent = autor;
+  document.getElementById(rbId).textContent = 'Remove';
 }
 
-newBook();
-
+const addNewBook = document.querySelector('.add-btn');
+addNewBook.addEventListener('click', () => {
+  newBook();
+});
