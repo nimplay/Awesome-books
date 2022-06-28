52d761ad21978c0acfcc0ad3f17ef61b9fc41916
nimplay <nimrod7day@gmail.com>
2022-06-28 12:04:30 -0400

    fix remain data in to the browser

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const title = document.querySelector('.title-book');
 const author = document.querySelector('.author-name');
-const bookData = [];
+const NbookData = [];
 let i = 0;
 
 /* add new book */
@@ -45,12 +45,19 @@ function newBook() {
 
 /* save into local storage */
 const addUserData = () => {
+  let savedData = localStorage.getItem('BookData');
+  savedData = JSON.parse(savedData);
   const dataUserObject = {
     name: author.value,
     title: title.value,
   };
-  bookData.push(dataUserObject);
-  localStorage.setItem('BookData', JSON.stringify(bookData));
+  if (savedData !== null) {
+    savedData.push(dataUserObject);
+    localStorage.setItem('BookData', JSON.stringify(savedData));
+  } else {
+    NbookData.push(dataUserObject);
+    localStorage.setItem('BookData', JSON.stringify(NbookData));
+  }
 };
 
 /* button form */
@@ -65,48 +72,50 @@ addNewBook.addEventListener('click', () => {
 function getUserData() {
   let data = localStorage.getItem('BookData');
   data = JSON.parse(data);
-  for (let j = 0; j < data.length; j += 1) {
-    const deck = document.getElementById('books-container');
-    const bookData = document.createElement('div');
-    const bookTitle = document.createElement('p');
-    const bookAuthor = document.createElement('p');
-    const removeBtn = document.createElement('button');
-    const line = document.createElement('div');
-    const booksaved = data[j].title;
-    bookData.className = 'book';
-    deck.appendChild(bookData);
+  if (data !== null) {
+    for (let j = 0; j < data.length; j += 1) {
+      const deck = document.getElementById('books-container');
+      const bookData = document.createElement('div');
+      const bookTitle = document.createElement('p');
+      const bookAuthor = document.createElement('p');
+      const removeBtn = document.createElement('button');
+      const line = document.createElement('div');
+      const booksaved = data[j].title;
+      bookData.className = 'book';
+      deck.appendChild(bookData);
 
-    bookTitle.classList.add('book-title');
-    bookAuthor.classList.add('book-author');
-    removeBtn.classList.add('remove-btn');
-    line.classList.add('line');
+      bookTitle.classList.add('book-title');
+      bookAuthor.classList.add('book-author');
+      removeBtn.classList.add('remove-btn');
+      line.classList.add('line');
 
-    bookTitle.setAttribute('id', `bt${i}`);
-    bookAuthor.setAttribute('id', `ba${i}`);
-    removeBtn.setAttribute('id', `rb${i}`);
-    i += 1;
-    const btId = bookTitle.getAttribute('id');
-    const baId = bookAuthor.getAttribute('id');
-    const rbId = removeBtn.getAttribute('id');
+      bookTitle.setAttribute('id', `bt${i}`);
+      bookAuthor.setAttribute('id', `ba${i}`);
+      removeBtn.setAttribute('id', `rb${i}`);
+      i += 1;
+      const btId = bookTitle.getAttribute('id');
+      const baId = bookAuthor.getAttribute('id');
+      const rbId = removeBtn.getAttribute('id');
 
-    bookData.appendChild(bookTitle);
-    bookData.appendChild(bookAuthor);
-    bookData.appendChild(removeBtn);
-    bookData.appendChild(line);
+      bookData.appendChild(bookTitle);
+      bookData.appendChild(bookAuthor);
+      bookData.appendChild(removeBtn);
+      bookData.appendChild(line);
 
-    document.getElementById(btId).textContent = data[j].title;
-    document.getElementById(baId).textContent = data[j].name;
-    document.getElementById(rbId).textContent = 'Remove';
+      document.getElementById(btId).textContent = data[j].title;
+      document.getElementById(baId).textContent = data[j].name;
+      document.getElementById(rbId).textContent = 'Remove';
 
-    removeBtn.addEventListener('click', () => {
-      bookData.remove();
-      for (let k = 0; k < data.length; k += 1) {
-        if (booksaved === data[k].title) {
-          data.splice(k, 1);
-          localStorage.setItem('BookData', JSON.stringify(data));
+      removeBtn.addEventListener('click', () => {
+        bookData.remove();
+        for (let k = 0; k < data.length; k += 1) {
+          if (booksaved === data[k].title) {
+            data.splice(k, 1);
+            localStorage.setItem('BookData', JSON.stringify(data));
+          }
         }
-      }
-    });
+      });
+    }
   }
 }
 getUserData();
